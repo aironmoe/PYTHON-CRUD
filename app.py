9100a35b07a710de1f1a15e73e24f790ebd852cb
aironmoe <oquindoairon@gmail.com>
2025-08-05 11:10:37 +0800

    second commit with database

--- app.py
+++ app.py
@@ -2,7 +2,13 @@ from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config[SQLALCHEMY_DATABASE_URl]
+app.config[SQLALCHEMY_DATABASE_URl] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.model):
+    id = db.Column(db.Integer, primary_key = true)
+    content = db.Column(db.String(200), nullable = flase)
+    commpleted = db.Column(db.Integer, default = 0)
 
 @app.route('/')
 
