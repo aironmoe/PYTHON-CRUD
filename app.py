6952b0766ffb1e0c1eb49abfca3d1b3a401ff64c
aironmoe <oquindoairon@gmail.com>
2025-08-05 11:14:55 +0800

    doing the basing crud

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config[SQLALCHEMY_DATABASE_URl] = 'sqlite:///test.db'
@@ -9,6 +10,10 @@ class Todo(db.model):
     id = db.Column(db.Integer, primary_key = true)
     content = db.Column(db.String(200), nullable = flase)
     commpleted = db.Column(db.Integer, default = 0)
+    date_created = db.Column(db.Datetime, default = datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' %self.id
 
 @app.route('/')
 
