db6b3cae98c6b8033e725778d01c347e89cca54a
aironmoe <oquindoairon@gmail.com>
2025-08-07 15:59:58 +0800

    adding auh.py

--- auth.py
+++ auth.py
@@ -9,4 +9,6 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=true)
     firstname = db.Column(db.String(400), nullable= False)
+    email = db.Column(db.String(400), nullable= False)
+    address = n(db.String(400), nullable= False)
     
