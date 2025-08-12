18964e2017082e6b59477ec7ae21e21e1566d0a6
aironmoe <oquindoairon@gmail.com>
2025-08-12 10:35:35 +0800

    naka gawa na din  ng users table para sa registration

--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -18,6 +18,25 @@ class Users(db.Model):
 
     def __repr__(self):
         return '<users %r>' % self.id
+
+@app.route('/' method=['POST', 'GET'])
+def index():
+    if request.methods == 'POST':
+        first_name = request.form['firstname']
+        new_name = Users(firstname=first_name)
+
+        try:
+            db.session.add(first_name)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding the task'
+    else
+    fname = Users.query.order_by(Todo.date_created).all()
+    return render_template('registration.html', fname=fname)
+
+
+
 
 if __name__ == '__main__':
     with app.app_context():
